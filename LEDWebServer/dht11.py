bafb8ee687cfd326bc8a65b1d48c869e5a19b8fa
Astour Krikor <72988512+KrikorAstour@users.noreply.github.com>
2022-10-17 14:03:06 -0700

    added dht11.py

---
+++ LEDWebServer/dht11.py
@@ -0,0 +1,19 @@
+import Adafruit_DHT
+import time
+ 
+DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_PIN = 18
+
+#     check temp
+#     if higher than 24 send email
+#     receive email
+#     if yes
+#     code for dc motor here
+while True:
+    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    if humidity is not None and temperature is not None:
+        print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
+
+#     else:
+#         print("Sensor failure. Check wiring.");
+    time.sleep(3);
